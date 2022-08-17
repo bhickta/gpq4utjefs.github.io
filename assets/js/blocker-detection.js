38ff1184ce919470534245369f45e8a1404b2f18
gpQ4utjefs <mail.himgk@gmail.com>
2022-08-17 21:57:12 +0530

    Testing js via alert

--- assets/js/blocker-detection.js
+++ assets/js/blocker-detection.js
@@ -1,31 +1,34 @@
-async function checkForAdBlocker() {
-    let Blocked;
+// async function checkForAdBlocker() {
+//     let Blocked;
 
-    async function Request() {
-        try {
-            return fetch(
-                    new Request(
-                        "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js", {
-                            method: 'HEAD',
-                            mode: 'no-cors'
-                        }))
-                .then(function (response) {
-                    // There is no AdBlocker
-                    Blocked = false;
-                    return Blocked;
-                }).catch(function (e) {
-                    // Failed, Because of an AdBlocker
-                    Blocked = true;
-                    return Blocked;
-                });
-        } catch (error) {
-            console.log(error);
-            Blocked = true;
-            return Blocked;
-        }
-    }
+//     async function Request() {
+//         try {
+//             return fetch(
+//                     new Request(
+//                         "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js", {
+//                             method: 'HEAD',
+//                             mode: 'no-cors'
+//                         }))
+//                 .then(function (response) {
+//                     // There is no AdBlocker
+//                     Blocked = false;
+//                     return Blocked;
+//                 }).catch(function (e) {
+//                     // Failed, Because of an AdBlocker
+//                     Blocked = true;
+//                     return Blocked;
+//                 });
+//         } catch (error) {
+//             console.log(error);
+//             Blocked = true;
+//             return Blocked;
+//         }
+//     }
 
-    return Blocked !== undefined ? Blocked : await Request();
-}
+//     return Blocked !== undefined ? Blocked : await Request();
+// }
 
-const usingBlocker = await checkForAdBlocker();
+// const usingBlocker = await checkForAdBlocker();
+
+// the hello world program
+alert("Hello, World!");
