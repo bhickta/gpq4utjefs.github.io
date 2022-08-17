f2f3de93dd924eb903860c938d654023193b8f86
gpQ4utjefs <mail.himgk@gmail.com>
2022-08-17 20:36:52 +0530

    Tst

---
+++ assets/js/disablecopy.js
@@ -0,0 +1,8 @@
+<script type="text/javascript">
+document.oncontextmenu = new Function("return false");
+const disableselect = (e) => {
+        return false
+}
+document.onselectstart = disableselect
+document.onmousedown = disableselect
+</script>
